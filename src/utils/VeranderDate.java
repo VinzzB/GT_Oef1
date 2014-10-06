7fa3d68cc2d15304daaade478a56b041c349a64b
Silvia24 <silvia.mirisola@gmail.com>
2014-10-06 18:19:41 +0200

    update voor negatieve getallen

--- src/utils/VeranderDate.java
+++ src/utils/VeranderDate.java
@@ -12,13 +12,14 @@ public class VeranderDate {
 	private int year;
 
 	public VeranderDate(NormalDate date1, int aantalDagen) {
-		// Switch Min / Max
-		NormalDate Maxdate = aantalDagen > 0 ? null : date1;
-		NormalDate Mindate = aantalDagen > 0 ? date1 : null;
-		if (Maxdate == null) {
-			day = Mindate.getDay();
-			month = Mindate.getMonth();
-			year = Mindate.getYear();
+
+		day = date1.getDay();
+		month = date1.getMonth();
+		year = date1.getYear();
+		if (aantalDagen > 0) {
+			// day = date.getDay();
+			// month = Mindate.getMonth();
+			// year = Mindate.getYear();
 
 			for (int i = 1; i <= aantalDagen; i++) {
 				if (day < NormalDate.getDagenInMaand(month, year)) {
@@ -39,29 +40,29 @@ public class VeranderDate {
 				}
 
 			}
-		} else {
-			day = Maxdate.getDay();
-			month = Maxdate.getMonth();
-			year = Maxdate.getYear();
-
-			for (int i = aantalDagen; i <= 0; i--) {
-				if (day == 1) {
-					if (month > 1) {
-						month--;
-					} else {
-						month = 12;
-						year--;
-					}
-					day = NormalDate.getDagenInMaand(month, year);
+		}
+		if (aantalDagen < 0) {
+			int j = Math.abs(aantalDagen);
+			for (int i = j; i > 0; i--) {
+				if (day <= NormalDate.getDagenInMaand(month, year) && day != 1) {
+					day--;
 				} else {
-					if (day > 1 && day < NormalDate.getDagenInMaand(month, year)) {
-						day--;
+					if (day == 1) {
+						if (month > 1) {
+							month--;
+						} else {
+							if (month == 1) {
+								month = 12;
+								year--;
+							}
+						}
+						day = NormalDate.getDagenInMaand(month, year);
 					}
 				}
 
-			}
-		}
-	}
+			} // end for
+		} // end if
+	} // end verander date
 
 	public int getDay() {
 		return day;
