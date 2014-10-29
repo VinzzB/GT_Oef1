2cd9d23c52eb27e16e75e294ba2cc4f60abc2fd5
Silvia24 <silvia.mirisola@gmail.com>
2014-10-29 22:19:41 +0100

    Create QuizDeelname.java

---
+++ src/model/QuizDeelname.java
@@ -0,0 +1,72 @@
+package model;
+import utils.Datum;
+/**
+ *
+ * @author Silvia
+ */
+public class QuizDeelname
+{
+ private Leerling owner;
+ private OpdrachtAntwoord opdrachtAntwoord;
+ private Datum datumDeelname;
+ private Quiz quiz;
+
+    public QuizDeelname()
+    {
+        this.owner = new Leerling();
+        this.opdrachtAntwoord = new OpdrachtAntwoord();
+        this.datumDeelname = new Datum();
+        this.quiz = new Quiz();
+    }     
+    public QuizDeelname(Leerling owner, OpdrachtAntwoord opdrachtAntwoord, Datum datumDeelname, Quiz quiz)
+    {
+        this.owner = owner;
+        this.opdrachtAntwoord = opdrachtAntwoord;
+        this.datumDeelname = datumDeelname;
+        this.quiz = quiz;
+    }
+
+    public void setOwner(Leerling newOwner)
+    {
+        Leerling old = owner;
+        owner = newOwner;
+        if (newOwner != null)
+        {
+            newOwner.addQuizDeelname(this);
+        }
+        if (old != null)
+        {
+            old.removeQuizDeelname(this);
+        }
+    }
+
+    public OpdrachtAntwoord getOpdrachtAntwoord()
+    {
+        return opdrachtAntwoord;
+    }
+
+    public void setOpdrachtAntwoord(OpdrachtAntwoord opdrachtAntwoord)
+    {
+        this.opdrachtAntwoord = opdrachtAntwoord;
+    }
+
+    public Datum getDatumDeelname()
+    {
+        return datumDeelname;
+    }
+
+    public void setDatumDeelname(Datum datumDeelname)
+    {
+        this.datumDeelname = datumDeelname;
+    }
+
+    public Quiz getQuiz()
+    {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz)
+    {
+        this.quiz = quiz;
+    }
+}
