735f7d7fd68c61c5e5d93206aa0f0d134cf16d98
memamo <Isaak@Java>
2014-11-05 00:14:57 +0100

    Mijne super complexe enum

---
+++ src/model/OpdrachtCategorie.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * Een enumeration voor de opdracht categoriën
+ * 
+ * @author Atlas
+ */
+public enum OpdrachtCategorie
+{
+	Aardrijkskunde,
+	Wiskunde,
+	Nederlands,
+	Wetenschappen
+}
