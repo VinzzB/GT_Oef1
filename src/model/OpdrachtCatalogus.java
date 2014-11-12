2e99e159909a3e9ba4ad7c69d6d7caa5707f2e1c
Natalia <fornnd@gmail.com>
2014-11-12 11:31:32 +0100

    OpdrachtCatalogus klasse van Nathalie teruggezet en nodige methoden toegevoegd

--- src/model/OpdrachtCatalogus.java
+++ src/model/OpdrachtCatalogus.java
@@ -2,6 +2,12 @@ package model;
 
 import java.util.ArrayList;
 
+/**
+ * @author      Nathalie Mathieu <natmathieu@gmail.com>
+ * @author      Natalia Dyubankova <fornnd@gmail.com>
+ * @version     1.0                 
+ * @since       2014-11-12  
+ */
 public class OpdrachtCatalogus
 {
 	private ArrayList<Opdracht> opdrachtCatalogus;
@@ -23,13 +29,19 @@ public class OpdrachtCatalogus
 		opdrachtCatalogus.add(opdracht);
 	}
 	
-	protected void verwijderOpdracht(Opdracht opdracht)
+	protected void verwijderOpdracht(Opdracht opdracht) throws Exception
 	{
-		opdrachtCatalogus.remove(opdracht);
+		//enkel mogelijk als nog niet gekoppeld aan quiz
+		if (!opdracht.isGekoppeldAanQuiz())
+		{
+			this.opdrachtCatalogus.remove(opdracht);
+		}
+		else throw new Exception("Opdracht kan niet verwijderd worden, "
+								+ "want is reeds gelinkt aan een quiz.");
 	}
 	 public int setOpdrachtID()
 	 {
-		 return opdrachtCatalogus.size() + 1;
+		 return this.opdrachtCatalogus.size() + 1;
 	 }
 	 
 	public Opdracht getOpdracht(int opdrachtID)
