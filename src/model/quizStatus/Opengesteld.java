d1d86fa1a110fc8146334094362d07171ac5a316
Silvia24 <silvia.mirisola@gmail.com>
2014-12-01 21:01:53 +0100

    Update Opengesteld.java

--- src/model/quizStatus/Opengesteld.java
+++ src/model/quizStatus/Opengesteld.java
@@ -1,32 +1,26 @@
 package model.quizStatus;
 
-import java.util.Objects;
 import model.Quiz;
 
 public class Opengesteld extends QuizStatus
 {
-    private Quiz quiz;
-
-    public Opengesteld(Quiz q)
+    private static Opengesteld statusOpengesteld = new Opengesteld();
+    private Opengesteld(){}
+    public static Opengesteld instance()
     {
-        this.quiz = q;
+        return statusOpengesteld;
     }
-
-    public Opengesteld()
+    
+    @Override
+    public void geefLaatsteKans(Quiz q)
     {
-        quiz = null;
+        q.setStatus(LaatsteKans.instance());
     }
 
     @Override
-    public void geefLaatsteKans()
+    public void sluitQuizAf(Quiz q)
     {
-        quiz.setStatus(new LaatsteKans(quiz));
-    }
-
-    @Override
-    public void sluitQuizAf()
-    {
-        quiz.setStatus(new Afgesloten(quiz));
+        q.setStatus(Afgesloten.instance());
     }
 
     @Override
@@ -36,58 +30,8 @@ public class Opengesteld extends QuizStatus
     }
 
     @Override
-    public int hashCode()
-    {
-        int hash = 5;
-        hash = 29 * hash + Objects.hashCode(this.quiz);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (obj == null)
-        {
-            return false;
-        }
-        if (getClass() != obj.getClass())
-        {
-            return false;
-        }
-        final Opengesteld other = (Opengesteld) obj;
-        return Objects.equals(this.quiz, other.quiz);
-    }
-    
-    @Override
     public String toString()
     {
         return "Opengesteld";
-    }
-
-    @Override
-    public int compareTo(Object o)
-    {
-        if (o == null)
-        {
-            throw new NullPointerException("Object is null");
-        }
-
-        if (this.equals(o) == true)
-        {
-            return 0;
-        }
-        if (this.hashCode() > o.hashCode())
-        {
-            return 1;
-        } else
-        {
-            return -1;
-        }
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException
-    {
-        return super.clone();
     }
 }
