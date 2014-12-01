f9b5103904a8b5f670faff66d7cd85f87078fe24
Silvia24 <silvia.mirisola@gmail.com>
2014-12-01 21:00:33 +0100

    Update LaatsteKans.java

--- src/model/quizStatus/LaatsteKans.java
+++ src/model/quizStatus/LaatsteKans.java
@@ -1,26 +1,20 @@
 package model.quizStatus;
 
-import java.util.Objects;
 import model.Quiz;
 
 public class LaatsteKans extends QuizStatus
 {
-    private Quiz quiz;
-
-    public LaatsteKans(Quiz q)
+    private static LaatsteKans statusLaatsteKans = new LaatsteKans();
+    private LaatsteKans(){}
+    public static LaatsteKans instance()
     {
-        this.quiz = q;
-    }
-
-    public LaatsteKans()
-    {
-        quiz = null;
+        return statusLaatsteKans;
     }
 
     @Override
-    public void sluitQuizAf()
+    public void sluitQuizAf(Quiz q)
     {
-            quiz.setStatus(new Afgesloten(quiz));
+            q.setStatus(Afgesloten.instance());
     }
 
     @Override
@@ -30,58 +24,9 @@ public class LaatsteKans extends QuizStatus
     }
 
     @Override
-    public int hashCode()
-    {
-        int hash = 7;
-        hash = 13 * hash + Objects.hashCode(this.quiz);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (obj == null)
-        {
-            return false;
-        }
-        if (getClass() != obj.getClass())
-        {
-            return false;
-        }
-        final LaatsteKans other = (LaatsteKans) obj;
-        return Objects.equals(this.quiz, other.quiz);
-    }
-    
-    @Override
     public String toString()
     {
         return "Laatste Kans";
     }
 
-    @Override
-    public int compareTo(Object o)
-    {
-        if (o == null)
-        {
-            throw new NullPointerException("Object is null");
-        }
-
-        if (this.equals(o) == true)
-        {
-            return 0;
-        }
-        if (this.hashCode() > o.hashCode())
-        {
-            return 1;
-        } else
-        {
-            return -1;
-        }
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException
-    {
-        return super.clone();
-    }
 }
