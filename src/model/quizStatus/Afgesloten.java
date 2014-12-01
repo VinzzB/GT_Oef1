372f1f0e38c030101fcc339a2d7dd1e043f216f5
Silvia24 <silvia.mirisola@gmail.com>
2014-12-01 20:56:15 +0100

    Update Afgesloten.java

--- src/model/quizStatus/Afgesloten.java
+++ src/model/quizStatus/Afgesloten.java
@@ -1,76 +1,12 @@
 package model.quizStatus;
 
-import java.util.Objects;
-import model.Quiz;
-
 public class Afgesloten extends QuizStatus
 {
-    private Quiz quiz;
-
     
-    public Afgesloten(Quiz q)
-    {
-        this.quiz = q;
-    }
-
-    public Afgesloten()
-    {
-        quiz = null;
-    }
-    @Override
-    public int hashCode()
-    {
-        int hash = 7;
-        hash = 67 * hash + Objects.hashCode(this.quiz);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if (obj == null)
-        {
-            return false;
-        }
-        if (getClass() != obj.getClass())
-        {
-            return false;
-        }
-        final Afgesloten other = (Afgesloten) obj;
-        return Objects.equals(this.quiz, other.quiz);
-    }
-
-    @Override
-    public String toString()
-    {
-        return "Afgesloten";
-    }
-    
-    @Override
-    public int compareTo(Object o)
-    {
-        if (o == null)
-        {
-            throw new NullPointerException("Object is null");
-        }
-
-        if (this.equals(o) == true)
-        {
-            return 0;
-        }
-        if (this.hashCode() > o.hashCode())
-        {
-            return 1;
-        } else
-        {
-            return -1;
-        }
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException
-    {
-        return super.clone();
-    }
+  private static final Afgesloten statusAfgesloten = new Afgesloten();
+  
+  public static QuizStatus instance()
+  {
+      return statusAfgesloten;
+  }
 }
-
